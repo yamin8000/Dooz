cac4281f644b56fbdee61b1de3d07f35402eb71c
YaMiN <yamin8000@yahoo.com>
2022-08-25 21:12:29 +0430

    migrated to material 3 theme system

--- app/src/main/java/io/github/yamin8000/dooz/MainActivity.kt
+++ app/src/main/java/io/github/yamin8000/dooz/MainActivity.kt
@@ -23,13 +23,6 @@ package io.github.yamin8000.dooz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.yamin8000.dooz.ui.theme.DoozTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,27 +30,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DoozTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DoozTheme {
-        Greeting("Android")
     }
 }
