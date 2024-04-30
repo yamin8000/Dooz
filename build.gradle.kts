1a3f33d49dc3fe3bfea0f53aa9a5c080e81c5d4c
Yamin Siahmargooei <yamin8000@yahoo.com>
2024-05-01 01:27:46 +0330

    minor fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@
  *     along with Dooz.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("com.android.library") version "8.4.0" apply false
